ca8d489a1a9b99d216d6eba716b8797f3f62fcdc
Isaac Jon <isaac.jon72@gmail.com>
2026-02-09 21:12:00 -0500

    finihsed

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num0, num1) {
+    if(!Number.isInteger(num0) || !Number.isInteger(num1)) return "ERROR";
+    if(num0 < 0 || num1 < 0 ) return "ERROR";
+    let left = Math.min(num0, num1);
+    let right = Math.max(num0, num1);
+    let ans = 0;
+    for(let i=left; i <= right; i++) {
+        ans += i;
+    }
+    return ans;
 };
 
 // Do not edit below this line
