7d4b8793adb2c7fe0a52a46c50a86c5502021e47
Isaac Jon <isaac.jon72@gmail.com>
2026-02-09 21:02:26 -0500

    finished

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...nums) {
+    for(const num of nums) {
+        arr = arr.filter((n) => n !== num);
+    }
+    return arr;
 };
+
+// console.log(removeFromArray([1, 2, 3], "1", 3));
 
 // Do not edit below this line
 module.exports = removeFromArray;
