802d54f0462cbc2f72a0167f098abf27042a7686
Isaac Jon <isaac.jon72@gmail.com>
2026-02-13 18:34:07 -0500

    13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,6 +1,13 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    str = str.replaceAll(' ', '').replaceAll('.', "").replaceAll(',', "").replaceAll('!', '');
+    let left = 0, right = str.length - 1;
+    while(left <= right) {
+        if(str.charAt(left++).toLowerCase() !== str.charAt(right--).toLowerCase()) return false;
+    }
+    return true;
 };
+
+console.log(palindromes('A car, a man, a maraca.'));
 
 // Do not edit below this line
 module.exports = palindromes;
