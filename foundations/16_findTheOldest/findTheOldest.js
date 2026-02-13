db9632ac86fb4991a88874ca9b4d3444d8d96754
Isaac Jon <isaac.jon72@gmail.com>
2026-02-13 18:51:50 -0500

    16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.reduce((total, curr) => {
+        let age0 = 0;
+        if(total.yearOfDeath === undefined) {
+            age0 = 2026 - total.yearOfBirth;
+        } else age0 = total.yearOfDeath - total.yearOfBirth;
+        let age1 = 0;
+        if(curr.yearOfDeath === undefined) {
+            age1 = 2026 - curr.yearOfBirth;
+        } else age1 = curr.yearOfDeath - curr.yearOfBirth; 
+        return age1 > age0 ? curr : total;    
+    })
 };
 
 // Do not edit below this line
