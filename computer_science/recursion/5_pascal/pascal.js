76694250a107be435bc669bebaf7275e2da1de10
Isaac Jon <isaac.jon72@gmail.com>
2026-03-25 16:54:44 -0400

    Finished

--- computer_science/recursion/5_pascal/pascal.js
+++ computer_science/recursion/5_pascal/pascal.js
@@ -1,5 +1,12 @@
-const pascal = function() {
-  
+const pascal = function(n) {
+    if(n === 1) return [1];
+    const prev = pascal(n-1);
+    const arr = [1];
+    for(let i=0; i<prev.length-1; i++) {
+        arr.push(prev[i] + prev[i+1]);
+    }
+    arr.push(1);
+    return arr;
 };
   
 // Do not edit below this line
