3ea8fe41ee69b421b0751d94653e0d7322124230
Isaac Jon <isaac.jon72@gmail.com>
2026-03-25 16:44:43 -0400

    finshed

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,21 @@
-const permutations = function() {
-  
+const permHelp = function(arr, curr, ans) {
+    if(arr.length === curr.length) ans.push(curr.slice());
+    for(let i=0; i<arr.length; i++) {
+        if(arr[i] != 0) {
+            const now = arr[i];
+            curr.push(arr[i]);
+            arr[i] = 0;
+            permHelp(arr, curr, ans);
+            curr.pop();
+            arr[i] = now;
+        }
+    }
+}
+
+const permutations = function(arr) {
+    const ans = [];
+    permHelp(arr, [], ans);
+    return ans;
 };
   
 // Do not edit below this line
